8c8efc36391d8d043eb072c8e261aff90a576bf1
nadav <nadavcremisi@gmail.com>
2026-03-20 17:48:22 +0200

    first commit

---
+++ src/main/java/com/kremozo/urlshortener/model/UrlEntity.java
@@ -0,0 +1,61 @@
+package com.kremozo.urlshortener.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+public class UrlEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(nullable = false)
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true)
+    private String shortCode;
+
+    private LocalDateTime createdAt;
+
+    public UrlEntity(){
+    }
+
+    public UrlEntity(String originalUrl,String shortCode){
+        this.originalUrl = originalUrl;
+        this.shortCode=shortCode;
+        this.createdAt=LocalDateTime.now();
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
